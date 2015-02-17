8195b3921b388730f3da02828186d09d8b162228
pearmany <pearmany@gmail.com>
2015-02-17 15:26:23 -0800

    The Third Test

--- BowlingKata_Ruby/BowlingGameTest.rb
+++ BowlingKata_Ruby/BowlingGameTest.rb
@@ -7,11 +7,6 @@ class TestGame < Test::Unit::TestCase
     @g = Game.new
   end
   
-  def testGutterGame
-    rollMany(20, 0)
-    assert_equal(0, @g.score())
-  end
-  
   def rollMany(n, pins)
     @i = 0
     while @i < n do
@@ -20,9 +15,22 @@ class TestGame < Test::Unit::TestCase
   end
 end
 
+  def testGutterGame
+    rollMany(20, 0)
+    assert_equal(0, @g.score())
+  end
+  
+
   def testAllOnes
     rollMany(20, 1)
     assert_equal(20, @g.score())
   end
   
-end
+  def testOneSpare
+    @g.roll(5)
+    @g.roll(5) # spare
+    @g.roll(3)
+    rollMany(17, 0)
+    assert_equal(16, @g.score())
+  end
+end #class
