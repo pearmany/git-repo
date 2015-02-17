1a793f3f3d2b7c74414c247e9ed7e81bf701fcb0
pearmany <pearmany@gmail.com>
2015-02-17 15:36:14 -0800

    The Third Test

--- BowlingKata_Ruby/BowlingGame.rb
+++ BowlingKata_Ruby/BowlingGame.rb
@@ -2,10 +2,13 @@ class Game
 
   def initialize
   @score = 0
+  @rolls = []
+  @currentRoll = 0
   end
 
   def roll(pins)
     @score += pins
+    @rolls[@currentRoll +=1] = pins
   end
 
   def score
