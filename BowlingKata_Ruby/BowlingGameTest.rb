f4f72ff3e832b20249f5c535802fcf5f32f85979
pearmany <pearmany@gmail.com>
2015-02-17 15:07:48 -0800

    The Second Test

--- BowlingKata_Ruby/BowlingGameTest.rb
+++ BowlingKata_Ruby/BowlingGameTest.rb
@@ -10,11 +10,17 @@ class TestGame < Test::Unit::TestCase
   def testGutterGame
     @n = 20
     @pins = 0
-  20.times do
-  @g.roll(@pins)
-  end
+  rollMany(@n, @pins)
   assert_equal(0, @g.score())
   end
+  
+  def rollMany(n, pins)
+    @i = 0
+    while @i < @n do
+      @i += 1
+    @g.roll(@pins)
+  end
+end
 
   def testAllOnes
   20.times do
