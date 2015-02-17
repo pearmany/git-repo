040fb9681968e9e38842b82b681cb6ab512699b5
pearmany <pearmany@gmail.com>
2015-02-17 05:34:04 -0800

    The Second Test

--- BowlingKata_Ruby/BowlingGameTest.rb
+++ BowlingKata_Ruby/BowlingGameTest.rb
@@ -2,17 +2,21 @@ require_relative "BowlingGame"
 require "test/unit"
  
 class TestGame < Test::Unit::TestCase
-  
+
   def testGutterGame
-  g = Game.new
+  g= Game.new
+  20.times do
   g.roll(0)
+  end
   assert_equal(0, g.score())
   end
-  
+
   def testAllOnes
-    g = Game.new
-    g.roll(1)
-    asser_equal(20, g.score())
+  g= Game.new
+  20.times do
+  g.roll(1)
+  end
+  assert_equal(20, g.score())
   end
   
 end
