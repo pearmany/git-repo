60ac5bc3fc8d0fe8bd545e2cb227905cc046721e
pearmany <pearmany@gmail.com>
2015-02-24 16:03:29 -0800

    the third test

--- BowlingKata_Ruby/BowlingGame.rb
+++ BowlingKata_Ruby/BowlingGame.rb
@@ -12,15 +12,15 @@ class Game
   def score
     @score = 0
     @frame = 0
-    @i = 0
+    @frameIndex = 0
     
     while @frame < 10
-      if @rolls[@i] + @rolls[@i + 1] == 10
-        @score += 10 + @rolls[@i + 2]
-        @i += 2
+      if @rolls[@frameIndex] + @rolls[@frameIndex + 1] == 10
+        @score += 10 + @rolls[@frameIndex + 2]
+        @frameIndex += 2
       else
-        @score += @rolls[@i] + @rolls[@i +1]
-        @i += 2
+        @score += @rolls[@frameIndex] + @rolls[@frameIndex +1]
+        @frameIndex += 2
       end
       @frame += 1
     end
