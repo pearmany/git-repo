c41f42e45e03990cd82e1244eecfe929e78392c9
pearmany <pearmany@gmail.com>
2015-02-24 16:14:31 -0800

    the third test

--- BowlingKata_Ruby/BowlingGameTest.rb
+++ BowlingKata_Ruby/BowlingGameTest.rb
@@ -27,10 +27,14 @@ end
   end
   
   def testOneSpare
-    @g.roll(5)
-    @g.roll(5) # spare
+    rollSpare()
     @g.roll(3)
     rollMany(17, 0)
     assert_equal(16, @g.score())
+  end
+  
+  def rollSpare()
+    @g.roll(5)
+    @g.roll(5)
   end
 end #class
