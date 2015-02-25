3007b61defe2894ce85562a0a0a0bb41e6d81c83
pearmany <pearmany@gmail.com>
2015-02-24 16:00:13 -0800

    The Third Test

--- BowlingKata_Ruby/BowlingGame.rb
+++ BowlingKata_Ruby/BowlingGame.rb
@@ -15,9 +15,14 @@ class Game
     @i = 0
     
     while @frame < 10
-    @score += @rolls[@i] + @rolls[@i + 1]
-    @i += 2
-    @frame += 1
+      if @rolls[@i] + @rolls[@i + 1] == 10
+        @score += 10 + @rolls[@i + 2]
+        @i += 2
+      else
+        @score += @rolls[@i] + @rolls[@i +1]
+        @i += 2
+      end
+      @frame += 1
     end
     @score
   end
