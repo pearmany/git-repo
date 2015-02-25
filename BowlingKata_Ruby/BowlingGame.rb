7cd9dc314689cc6e5b367a52e18b29677e6c720b
pearmany <pearmany@gmail.com>
2015-02-24 16:12:24 -0800

    the third test

--- BowlingKata_Ruby/BowlingGame.rb
+++ BowlingKata_Ruby/BowlingGame.rb
@@ -12,18 +12,22 @@ class Game
   def score
     @score = 0
     @frame = 0
-    @frameIndex = 0
+    frameIndex = 0
     
     while @frame < 10
-      if @rolls[@frameIndex] + @rolls[@frameIndex + 1] == 10
-        @score += 10 + @rolls[@frameIndex + 2]
-        @frameIndex += 2
+      if isSpare?(frameIndex)
+        @score += 10 + @rolls[frameIndex + 2]
+        frameIndex += 2
       else
-        @score += @rolls[@frameIndex] + @rolls[@frameIndex +1]
-        @frameIndex += 2
+        @score += @rolls[frameIndex] + @rolls[frameIndex +1]
+        frameIndex += 2
       end
       @frame += 1
     end
     @score
+  end
+  
+  def isSpare?(frameIndex)
+    @rolls[frameIndex] + @rolls[frameIndex + 1] == 10
   end
 end #class
